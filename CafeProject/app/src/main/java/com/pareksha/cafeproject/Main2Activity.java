7ce24ee6a3cfd96cdbb7933c7aea83acc5f6bc66
pareksha <pareksha.manchanda@gmail.com>
2017-10-05 23:58:37 +0530

    CoffeeShop_firstAndroidProject

---
+++ CafeProject/app/src/main/java/com/pareksha/cafeproject/Main2Activity.java
@@ -0,0 +1,47 @@
+package com.pareksha.cafeproject;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DialogTitle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Main2Activity.this);
+                builder.setMessage("Do you want to proceed?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast toast = Toast.makeText(getApplicationContext(),"Request for booking table has been made",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                builder.show();
+            }
+        });
+    }
+}
---
+++ CafeProject/app/src/main/java/com/pareksha/cafeproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.pareksha.cafeproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+
+    }
+}
