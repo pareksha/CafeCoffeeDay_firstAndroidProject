ab0721f4d4ebf8f9a82ad9a7a499d47ac9c4bc3d
pareksha <pareksha.manchanda@gmail.com>
2017-10-13 23:21:39 +0530

    Final Commit

--- CafeProject/app/src/main/java/com/pareksha/cafeproject/Main3Activity.java
+++ CafeProject/app/src/main/java/com/pareksha/cafeproject/Main3Activity.java
@@ -2,6 +2,8 @@ package com.pareksha.cafeproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.facebook.com");
+
     }
 }
--- CafeProject/app/src/main/java/com/pareksha/cafeproject/MainActivity.java
+++ CafeProject/app/src/main/java/com/pareksha/cafeproject/MainActivity.java
@@ -3,27 +3,86 @@ package com.pareksha.cafeproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import static com.pareksha.cafeproject.R.id.btn;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    Toolbar toolbar;
+    ImageView imageView;
+    Menu menu;
+    MenuItem item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.btn);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        String Url = "http://coffeedrinkersgear.net/wp-content/uploads/2016/08/image-20160615-14016-njqw65.jpg";
+        Picasso.with(MainActivity.this)
+                .load(Url)
+                .placeholder(R.mipmap.ic_launcher)
+                .into(imageView);
+
+
+        Button btn = (Button) findViewById(R.id.btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+
             }
+
         });
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                startActivity(new Intent(MainActivity.this, Main3Activity.class));
+                break;
+            case R.id.item2:
+                break;
+
+            case R.id.item3:
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
     }
 }
+
+
+
+
+
+
+
+
